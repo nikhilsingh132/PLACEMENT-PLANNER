0ddfecb644eac3f5f54cabda2b3f6632ff62de4c
Soniya Karamchandani <soniyaKaramchandani02@gmail.com>
2023-05-10 11:11:46 +0530

    question list page

--- src/component/QuestionList.js
+++ src/component/QuestionList.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import "../styles/questionList.css";
 function QuestionList() {
   return (
-    <div>QuestionList</div>
-  )
+    <>
+      <div class="custom-ol">
+        <h1>GRAPH QUESTIONS LIST</h1>
+
+        <ol>
+          <li>
+            <a href="#createqueryschema"> custom ol li url link list</a>
+          </li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+          <li>custom ol li url link list</li>
+        </ol>
+      </div>
+    </>
+  );
 }
 
-export default QuestionList
+export default QuestionList;
